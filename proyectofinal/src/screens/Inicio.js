1ee1a0bc18418cc6e154c89b1917019beacbd8d1
jcukierholmberg <jcukierholmberg@udesa.edu.ar>
2021-11-11 16:34:38 -0300

    Instagramio

---
+++ proyectofinal/src/screens/Inicio.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import {Text, TouchableOpacity, View, StyleSheet, Image, ActivityIndicator, FlatList, TextInput} from 'react-native';
+import { db, auth } from '../firebase/config';
+import Post from '../components/Post';
+
+
+class Inicio extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      posteos: [],
+    }
+  }
+  componentDidMount(){
+    
+    db.collection('posts').onSnapshot(
+      docs => {
+        //Array para crear datos en formato más útil.
+        let posts = [];
+        docs.forEach( doc => {
+          posts.push({
+            id: doc.id,   
+            data: doc.data(),
+          })
+        })
+
+        this.setState({
+          posteos: posts,
+        })
+      }
+    )
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <FlatList 
+          data= { this.state.posteos }
+          keyExtractor = { post => post.id}
+          renderItem = { ({item}) => <Post postData={item} />} // <Text>{item.data.texto}</Text>//Podríamos armar un componente <Post > más complejo y rendirazolo con los datos de cada documanto.
+        />
+      </View>
+      )
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    paddingHorizontal:10,
+  },
+  formContainer:{
+    backgroundColor: '#ccc',
+    marginHorizontal: 10,
+    padding:10,
+  },
+  field:{
+    borderColor: '#444',
+    borderWidth:1,
+    borderStyle: 'solid',
+    height: 20,
+    paddingHorizontal: 20,
+    paddingVertical:10
+  },
+  image:{
+    height: 250,
+  },
+  touchable:{
+    backgroundColor: '#ccc',
+    borderRadius:4,
+    marginVertical:10,
+  }
+})
+
+export default Inicio;
